ad82cef69a7ef927829db769421b46d00325d3eb
Isaac Anderson <isaacaloysious@gmail.com>
2023-07-27 14:43:04 -0700

    tricky tricky part 2

--- main.cpp
+++ main.cpp
@@ -1,23 +1,11 @@
 // this was a lot trickier than I thought it would be. 
 // I had to use a lot of escape characters to get the output to work correctly. And learn about raw string literals.
-#include <iostream>
+
+#include <cstdio>
+
 int main() {
-    char q = 34; 
-    std::string program = "#include <iostream>\nint main() {\n    char q = 34;   \"\n    std::string program = ";
-    std::string program_end = ";\n    std::cout << program << q << program << q << program_end << std::endl;\n    return 0;\n}";
-    std::cout << program << q << program << q << program_end << std::endl;
-    return 0;
+    auto a = "int main() {auto a=%c%s%c; std::printf(a,34,a,34);}";
+    std::printf(a, 34, a, 34);
 }
 
-
-// Output
-// #include <iostream>
-// int main() {
-//     char q = 34;  "
-//     std::string program = "#include <iostream>
-// int main() {
-//     char q = 34;"
-//     std::string program = ";
-//     std::cout << program << q << program << q << program_end << std::endl;
-//     return 0;
-// }
+// int main() {auto a="int main() {auto a=%c%s%c; std::printf(a,34,a,34);}"; std::printf(a,34,a,34);}
