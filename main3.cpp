7d969caafd15a4e8adf7a87b5231bf9b7a161258
Isaac Anderson <isaacaloysious@gmail.com>
2023-07-28 07:38:35 -0700

    __File__ is cool

--- main3.cpp
+++ main3.cpp
@@ -4,6 +4,7 @@
 
 int main() {
     std::string line;
+    // __FILE__ made this a lot simpler
     std::ifstream file(__FILE__);
     while (std::getline(file, line)) {
         std::cout << line << std::endl;
