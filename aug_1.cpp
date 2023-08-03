92d5d1f3343baecdfbced225b069b2d718b8ac98
Isaac Anderson <isaacaloysious@gmail.com>
2023-08-03 14:26:44 -0700

    distance / overload <<

--- aug_1.cpp
+++ aug_1.cpp
@@ -17,8 +17,17 @@ public:
         Value mag = sqrt(_x*_x + _y*_y + _z*_z);
         return xyzVector(_x/mag, _y/mag, _z/mag);
     }
-
-
+    // references and pointers took up majority of this time for me, I had to look up how to do this. 
+    // however once references and pointers clicked a little bit it made many things clear
+    xyzVector distance(const xyzVector& vec) const {
+        return xyzVector(vec._x - _x, vec._y - _y, vec._z - _z);
+    }
+    // friend function to overload the << operator, this was the main trouble for me
+    // required a lot of psuedo code and trial and error to get this to work
+    friend std::ostream& operator<<(std::ostream& os, const xyzVector& vec) {
+        os << "[" << vec._x << ", " << vec._y << ", " << vec._z << "]";
+        return os;
+    }
 
 private:
     Value _x, _y, _z;
@@ -26,7 +35,9 @@ private:
 
 int main() {
     xyzVector<double> v1 = {0, 0, 0};
-    xyzVector<double> v2 = {2, 2, 2};
+    // xyzVector<double> v1 = {4,18,12};
+   xyzVector<double> v2 = {2, 2, 2};
+    // xyzVector<double> v2 = {3,8,102};
     xyzVector<double> v3 = v2 - v1;
     v3 = v3.normalize(); // compute the unit vector
     xyzVector<double> v4 = v3.distance(v2); // compute the cartesian difference
@@ -34,25 +45,3 @@ int main() {
     return 0;
 }
  
-
-
-
-/*
-
-                xyzVector<double> v1 = {0, 0, 0};
-
-                xyzVector<double> v2 = {2, 2, 2};
-
-                xyzVector<double> v3 = v2  - v1;
-
-                v3  = v3.norm(); // compute the unit vector
-
-                xyzVector<double> v4 = v3.distance(v2); // compute the cartesian difference
-
-                std::cout << v4 << std::endl; // should print out [x, y, z] where _x,_y,_z are the values inside v4;
-
-                return 0;
-
-               
-
-*/
